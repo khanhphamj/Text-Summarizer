ad3cd3a696a3aaa27ab2ffcb76e3c987c0b82b19
khanhphamj <phukhanh1903@gmail.com>
2024-05-13 12:36:06 +0700

    build_5: Upgrade html

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,6 @@ from app import app
 def home():
     if request.method == 'POST':
         text = request.form['text']
-        summary = summarizer(text, max_length=130, min_length=30, do_sample=False)
+        summary = summarizer(text, max_length=1000, min_length=30, do_sample=False)
         return render_template('summary.html', original_text=text, summary_text=summary[0]["summary_text"])
     return render_template('index.html')
